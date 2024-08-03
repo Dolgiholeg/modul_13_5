6a6372eac69e379f0f614b1c640c5f82f3d190d4
Dolgiholeg <Dolgiholeg@rambler.ru>
2024-08-03 09:36:11 +0300

    Добавление файлов 03.08.24

---
+++ modul_13_5.py
@@ -0,0 +1,75 @@
+from aiogram import Bot, Dispatcher, executor, types
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from aiogram.dispatcher.filters.state import State, StatesGroup
+from aiogram.dispatcher import FSMContext
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+import asyncio
+
+api = "7276421876:AAH99Xjc6JEbJOJDe2Mnj6rfrvdC1zF0wRQ"
+bot = Bot(token=api)
+dp = Dispatcher(bot, storage=MemoryStorage())
+
+class UserState(StatesGroup):
+    age = State()
+    growth = State()
+    weight = State()
+
+kb = ReplyKeyboardMarkup(resize_keyboard=True, row_width=2)
+button = KeyboardButton(text='Рассчитать')
+button2 = KeyboardButton(text='Информация')
+kb.add(button, button2)
+
+
+@dp.message_handler(commands=['start'])
+async def start(message):
+    await message.answer('Привет! Я бот помогающий твоему здоровью.', reply_markup=kb)
+
+@dp.message_handler(text='Информация')
+async def inform(message):
+    await message.answer('информация о боте!')
+@dp.message_handler(text = "Рассчитать")
+async def set_age(message):
+    await message.answer("Введите свой возраст:")
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+    await state.update_data(age=message.text)
+    await message.answer("Введите свой рост:")
+    await UserState.growth.set()
+
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+    await state.update_data(growth = message.text)
+    await message.answer("Введите свой вес:")
+    await UserState.weight.set()
+
+@dp.message_handler(state = UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    Result = 10 * int(data.get('weight')) + 6.25 * int(data.get('growth')) - 5 * int(data.get('age')) + 5
+    await message.answer(f"Ваша норма калорий: {Result}")
+    await state.finish()
+
+
+# kb = ReplyKeyboardMarkup()
+# button = KeyboardButton(text='Информация')
+# button2 = KeyboardButton(text='Начало')
+# kb.add(button)
+# kb.add(button2)
+#
+# @dp.message_handler(commands=['start'])
+# async def start(message):
+#     await message.answer('привет', reply_markup=kb)
+#
+# @dp.message_handler(text='Информация')
+# async def inform(message):
+#     await message.answer('информация о боте!')
+
+
+
+
+
+if __name__ == "__main__":
+    executor.start_polling(dp, skip_updates=True)
